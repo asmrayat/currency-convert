40dab3ee9de1c37705c41c12476f75f3e150bc45
saif-uddin-shrabon <saif06065@gmail.com>
2023-05-18 06:08:24 +0600

    logical site done

--- app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -5,11 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.badge.BadgeUtils;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView setting ;
+    Button Convert;
+    TextView FromText, ToText,Result;
+    EditText Amount;
+    double  amountResult;
 
 
     @Override
@@ -18,12 +27,70 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setting = findViewById(R.id.setting);
+        Convert = findViewById(R.id.convertBtn);
+        FromText = findViewById(R.id.textFrom);
+        ToText = findViewById(R.id.textSpacerNoTitle);
+        Result = findViewById(R.id.result);
+        Amount = findViewById(R.id.ammount);
+
+
 
         setting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, settingActivity.class));
 
+            }
+        });
+
+        FromText.setText(settingActivity.FromCurency);
+        ToText.setText(settingActivity.ToCurency);
+
+
+        Convert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                amountResult = Double.parseDouble(Amount.getText().toString());
+                System.out.println("getTK : "+amountResult);
+                if(settingActivity.FromCurency.equals("USD") && settingActivity.ToCurency.equals("JPY")){
+                    double conversionResult = amountResult * 137.60221;
+                    Result.setText(String.valueOf(conversionResult));
+                    System.out.println("getDollar : "+conversionResult);
+                } else if (settingActivity.FromCurency.equals("USD") && settingActivity.ToCurency.equals("CAD")) {
+                    double conversionResult = amountResult * 1.3468278;
+                    Result.setText(String.valueOf(conversionResult));
+                }
+                else if (settingActivity.FromCurency.equals("USD") && settingActivity.ToCurency.equals("EUR")) {
+                    double conversionResult = amountResult *0.92264332;
+                    Result.setText(String.valueOf(conversionResult));
+                }  else if (settingActivity.FromCurency.equals("CAD") && settingActivity.ToCurency.equals("USD")) {
+                    double conversionResult = amountResult * 0.7425036;
+                    Result.setText(String.valueOf(conversionResult));
+                } else if (settingActivity.FromCurency.equals("CAD") && settingActivity.ToCurency.equals("JPY")) {
+                    double conversionResult = amountResult * 102.17816;
+                    Result.setText(String.valueOf(conversionResult));
+                }else if (settingActivity.FromCurency.equals("CAD") && settingActivity.ToCurency.equals("EUR")) {
+                    double conversionResult = amountResult * 0.68510676;
+                    Result.setText(String.valueOf(conversionResult));
+                }else if (settingActivity.FromCurency.equals("JPY") && settingActivity.ToCurency.equals("USD")) {
+                    double conversionResult = amountResult * 0.0072666256;
+                    Result.setText(String.valueOf(conversionResult));
+                }else if (settingActivity.FromCurency.equals("JPY") && settingActivity.ToCurency.equals("CAD")) {
+                    double conversionResult = amountResult * 0.0097864086;
+                    Result.setText(String.valueOf(conversionResult));
+                }else if (settingActivity.FromCurency.equals("JPY") && settingActivity.ToCurency.equals("EUR")) {
+                    double conversionResult = amountResult * 0.0067051046;
+                    Result.setText(String.valueOf(conversionResult));
+                }else if (settingActivity.FromCurency.equals("EUR") && settingActivity.ToCurency.equals("USD")) {
+                    double conversionResult = amountResult * 1.0837916;
+                    Result.setText(String.valueOf(conversionResult));
+                }else if (settingActivity.FromCurency.equals("EUR") && settingActivity.ToCurency.equals("CAD")) {
+                    double conversionResult = amountResult * 1.4595774;
+                    Result.setText(String.valueOf(conversionResult));
+                }else if (settingActivity.FromCurency.equals("EUR") && settingActivity.ToCurency.equals("JPY")) {
+                    double conversionResult = amountResult * 149.15013;
+                    Result.setText(String.valueOf(conversionResult));
+                }
             }
         });
     }
--- app/src/main/java/com/example/currencyconverter/settingActivity.java
+++ app/src/main/java/com/example/currencyconverter/settingActivity.java
@@ -2,13 +2,60 @@ package com.example.currencyconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import java.security.PublicKey;
 
 public class settingActivity extends AppCompatActivity {
-
+    public RadioGroup FromGroup,ToGroup;
+   public static String FromCurency, ToCurency;
+   Button CurencyConfirmBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        FromGroup = findViewById(R.id.fromGroup);
+        ToGroup = findViewById(R.id.ToGroup);
+        CurencyConfirmBtn = findViewById(R.id.curencyConfirmBtn);
+        FromGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // Get the selected Radio Button
+                RadioButton radioButton = group.findViewById(checkedId);
+
+                // on below line we are setting text
+                // for our status text view.
+              //  statusTV.setText(radioButton.getText());
+                FromCurency = radioButton.getText().toString();
+                System.out.println("ChekButton: "+FromCurency);
+
+            }
+        });
+
+        ToGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // Get the selected Radio Button
+                RadioButton radioButton1 = group.findViewById(checkedId);
+
+                // on below line we are setting text
+                // for our status text view.
+                //  statusTV.setText(radioButton.getText());
+                ToCurency = radioButton1.getText().toString();
+                System.out.println("ChekButton: "+ToCurency);
+            }
+        });
+
+        CurencyConfirmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(settingActivity.this, MainActivity.class));
+            }
+        });
     }
 }
